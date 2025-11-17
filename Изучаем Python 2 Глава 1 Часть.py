ed7231517ee2820bb75163ef30da6b8410fb51a6
Pikulinaha <146559827+Pikulinaha@users.noreply.github.com>
2025-11-17 21:01:43 +0300

    Add files via upload

---
+++ Изучаем Python 2 Глава 1 Часть.py
@@ -0,0 +1,31 @@
+#Строки
+# message="heLlo worlD"
+# print(message.title())
+# print(message.upper())
+# print(message.lower())
+#f-строки
+# firs_name="Vladimir"
+# last_name="Almakaev"
+# full_name=f"{firs_name} {last_name}"
+# message=(f"Hello, {full_name.title()}!")
+# print(message)
+#Табуляции и разрывы строк
+#print("Languages:\n\tPython\n\tC\n\tJavaScript")
+#Удаление пропусков
+#favorite_language=' python '
+#favorite_language=favorite_language.rstrip()-справа
+#favorite_language=favorite_language.lstrip()-слева
+#favorite_language=favorite_language.strip()-со всех сторон
+#print(favorite_language)
+#Упражнение 2.3,4,5
+# name="eGor"
+
+# message=f"Hello {name.lower()},would you like to learn some Python today?"
+# print(message)
+# message=f"Hello {name.upper()},would you like to learn some Python today?"
+# print(message)
+# message=f"Hello {name.title()},would you like to learn some Python today?"
+# print(message)
+# name=" Eminem "
+# message='однажды сказал, \n\t"Запомни: великие не ждут подходящего момента. Они делают моменты великими."'
+# print(f"{name.strip()} {message}")
